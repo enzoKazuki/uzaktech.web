5bfd76812b015d742451cdeb71efae0d8ad0b9ae
enzoKazuki <enzo.kazuki9@gmail.com>
2026-08-20 00:30:45 -0300

    ImageShowCase fix safari 5

--- src/components/ProjectView/ImageShowCase/styles.ts
+++ src/components/ProjectView/ImageShowCase/styles.ts
@@ -2,39 +2,43 @@
 
 import { cornerBox, Span } from "@/styles/primitive";
 import styled from "styled-components";
-
-export const Img = styled.img`
-	position: relative;
-	cursor: pointer;
-	width: auto;
-	height: 100%;
-	max-height: 100%;
-	-webkit-user-drag: none;
-	z-index: 0;
+export const Root = styled.div<{$moving?: boolean}>`
+	height: max(calc(min(100dvw, calc(var(--max-width) - 300px)) / 3 / 1.5), 130px);
+	max-width: 100%;
+	padding: 1px;
+	margin: 13px 0 0;
+	display: flex;
+	flex-direction: row;
+	gap: 13px;
+	overflow: auto;
+	overflow-y: hidden;
+	scrollbar-color: ${({theme}) => `rgba(${theme.colorsRgbC.boxShadow}, 0.23)`} transparent;
+	cursor: ${(p) => p.$moving ? "crosshair" : "default"};
 `;
 
 export const Wrapper = styled.div<{$moving?: boolean}>`
 	position: relative;
+	height: 100%;
 	max-width: fit-content;
 	flex: 0 0 auto;
-	max-height: 100%;
-	height: 100%;
-	width: auto;
 	pointer-events: ${(p) => p.$moving ? "none" : "auto"};
 	outline: solid 1px ${(p) => p.theme.colors.boxShadow};
 
-	&:hover 
-	{
+	&:hover {
 		${(p) => cornerBox(p.theme)}
-
-		& > img {
-			filter: brightness(93%);
-		}
-
-		& > span {
-			opacity: 1;
-		}
+		& > img { filter: brightness(93%); }
+		& > span { opacity: 1; }
 	}
+`;
+
+export const Img = styled.img`
+	position: relative;
+	cursor: pointer;
+	height: 100%;
+	width: auto;
+	max-width: 100%;
+	-webkit-user-drag: none;
+	z-index: 0;
 `;
 
 export const HoverText = styled(Span).attrs({$size: "xv", $weight: "450", $colorPreset: "boxBackground"})`
@@ -49,18 +53,4 @@ export const HoverText = styled(Span).attrs({$size: "xv", $weight: "450", $color
 	opacity: 0;
 	pointer-events: none;
 	user-select: none;
-`;
-
-export const Root = styled.div<{$moving?: boolean}>`
-	max-height: max(calc(min(100dvw, calc(var(--max-width) - 300px)) / 3 / 1.5), 130px);
-	max-width: 100%;
-	padding: 1px;
-	margin: 13px 0 0;
-	display: flex;
-	flex-direction: row;
-	gap: 13px;
-	overflow: auto;
-	overflow-y: hidden;
-	scrollbar-color: ${({theme}) => `rgba(${theme.colorsRgbC.boxShadow}, 0.23)`} transparent;
-	cursor: ${(p) => p.$moving ? "crosshair" : "default"};
 `;
